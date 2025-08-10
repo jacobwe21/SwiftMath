19bca5e1d0e539570c50b0b511251c5022d5c0d2
Jacob Esselstyn <jacobwe21@gmail.com>
2025-08-10 18:30:10 -0400

    Improve accessibility

--- Sources/MySwiftMath/Measurements.swift
+++ Sources/MySwiftMath/Measurements.swift
@@ -1378,6 +1378,7 @@ public struct ENGRUnitPicker<EngrUnitType: EngineeringUnit, V>: View where EngrU
 			if unit.mixedUnitSystems {
 				ForEach(EngrUnitType.allEngineeringUnitSymbols, id: \.self) { unitSymbol in
 					Text(unitSymbol).tag(unitSymbol)
+						.accessibilityLabel(unit.description)
 				}
 			} else {
 				if !allowedUnitSystems.contains(.SI) && !unit.isImperial || !allowedUnitSystems.contains(.imperial) && unit.isImperial {
@@ -1386,11 +1387,13 @@ public struct ENGRUnitPicker<EngrUnitType: EngineeringUnit, V>: View where EngrU
 				if allowedUnitSystems.contains(.SI) {
 					ForEach(EngrUnitType.allSIEngineeringUnitSymbols, id: \.self) { unitSymbol in
 						Text(unitSymbol).tag(unitSymbol)
+							.accessibilityLabel(unit.description)
 					}
 				}
 				if allowedUnitSystems.contains(.imperial) {
 					ForEach(EngrUnitType.allImperialEngineeringUnitSymbols, id: \.self) { unitSymbol in
 						Text(unitSymbol).tag(unitSymbol)
+							.accessibilityLabel(unit.description)
 					}
 				}
 			}
